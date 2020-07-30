d93da2a0d3d5dc1f06efcf295aa1dc066dfaff27
Sandy Cash <lhcash@us.ibm.com>
2020-07-29 22:06:31 -0400

    Add more actions to CLI

--- cli/exec.go
+++ cli/exec.go
@@ -18,10 +18,32 @@ type SubCommand struct {
 	Token    string
 }
 
+type GroupMetadata struct {
+	Clusters Clusters
+}
+
 type SubscriptionMetadata struct {
 	ChannelUUID string
 	VersionUUID string
 	Groups      Groups
+}
+
+type VersionMetadata struct {
+	ChannelUUID string
+	Filename    string
+	Content     []byte
+	Description string
+}
+
+type Clusters []string
+
+func (c *Clusters) String() string {
+	return strings.Join(*c, ", ")
+}
+
+func (c *Clusters) Set(v string) error {
+	*c = append(*c, v)
+	return nil
 }
 
 type Groups []string
@@ -37,7 +59,9 @@ func (g *Groups) Set(v string) error {
 
 var (
 	subCmd               SubCommand
+	groupMetadata        GroupMetadata
 	subscriptionMetadata SubscriptionMetadata
+	versionMetadata      VersionMetadata
 )
 
 func TargetResource(resourceType string) {
@@ -60,14 +84,40 @@ func (cmd *SubCommand) execute(s *client.SatCon) (interface{}, error) {
 	)
 	switch cmd.Resource {
 
+	case TypeChannel:
+		switch cmd.Action {
+		case ActionAdd:
+			result, err = s.Channels.AddChannel(cmd.OrgID, cmd.Name, cmd.Token)
+		case ActionList:
+			result, err = s.Channels.Channels(cmd.OrgID, cmd.Token)
+		case ActionDelete:
+			fallthrough
+		case ActionRemove:
+			result, err = s.Channels.RemoveChannel(cmd.OrgID, cmd.Id, cmd.Token)
+		}
+
 	case TypeCluster:
 		switch cmd.Action {
 		case ActionList:
 			result, err = s.Clusters.ClustersByOrgID(cmd.OrgID, cmd.Token)
+		case ActionRemove:
+			fallthrough
 		case ActionDelete:
 			result, err = s.Clusters.DeleteClusterByClusterID(cmd.OrgID, cmd.Id, cmd.Token)
 		case ActionRegister:
 			result, err = s.Clusters.RegisterCluster(cmd.OrgID, types.Registration{Name: cmd.Name}, cmd.Token)
+		default:
+			err = fmt.Errorf("%s is not a recognized action for resource type %s", cmd.Action, cmd.Resource)
+		}
+
+	case TypeGroup:
+		switch cmd.Action {
+		case ActionAdd:
+			result, err = s.Groups.AddGroup(cmd.OrgID, cmd.Name, cmd.Token)
+		case ActionList:
+			result, err = s.Groups.Groups(cmd.OrgID, cmd.Token)
+		case ActionClusters:
+			result, err = s.Groups.GroupClusters(cmd.OrgID, cmd.Id, groupMetadata.Clusters, cmd.Token)
 		default:
 			err = fmt.Errorf("%s is not a recognized action for resource type %s", cmd.Action, cmd.Resource)
 		}
@@ -79,6 +129,29 @@ func (cmd *SubCommand) execute(s *client.SatCon) (interface{}, error) {
 		case ActionAdd:
 			result, err = s.Subscriptions.AddSubscription(cmd.OrgID, cmd.Name, subscriptionMetadata.ChannelUUID,
 				subscriptionMetadata.VersionUUID, subscriptionMetadata.Groups, cmd.Token)
+		default:
+			err = fmt.Errorf("%s is not a recognized action for resource type %s", cmd.Action, cmd.Resource)
+		}
+
+	case TypeVersion:
+		switch cmd.Action {
+		case ActionAdd:
+			if versionMetadata.Filename == "" {
+				err = fmt.Errorf("Must specify content file with -%s flag", FLAG_FILENAME)
+				break
+			}
+
+			versionMetadata.Content, err = MarshalYAMLFromFile(versionMetadata.Filename)
+			if err != nil {
+				err = fmt.Errorf("Unable to read content file %s: %s", versionMetadata.Filename, err)
+				break
+			}
+
+			result, err = s.Versions.AddChannelVersion(cmd.OrgID, versionMetadata.ChannelUUID, cmd.Name, versionMetadata.Content, versionMetadata.Description, cmd.Token)
+		case ActionDelete:
+			fallthrough
+		case ActionRemove:
+			result, err = s.Versions.RemoveChannelVersion(cmd.OrgID, cmd.Id, cmd.Token)
 		default:
 			err = fmt.Errorf("%s is not a recognized action for resource type %s", cmd.Action, cmd.Resource)
 		}
