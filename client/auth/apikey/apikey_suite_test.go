4633884e3b7ab1f0d70cc0ed136a2d7543a71490
Florian Froese <71638048+froesef@users.noreply.github.com>
2021-03-12 18:52:52 +0100

    Add support for authentication using an apikey of an OAuth service (#29)
    This is an open source Razee feature and is not officially supported by the project committers.

---
+++ client/auth/apikey/apikey_suite_test.go
@@ -0,0 +1,13 @@
+package apikey_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestAuth(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "API Key Auth Suite")
+}
