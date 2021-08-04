a71274e7b5075c94bbd73c42fb465bb2b554b77a
Steven Collins <Steven.Collins@ibm.com>
2021-08-04 10:32:11 -0500

    Fix minor typos, lint errors (#40)

--- client/actions/subscriptions/add_subscription_test.go
+++ client/actions/subscriptions/add_subscription_test.go
@@ -58,7 +58,7 @@ var _ = Describe("AddSubscription", func() {
 		})
 	})
 
-	Describe("AddSubcription", func() {
+	Describe("AddSubscription", func() {
 
 		var (
 			addSubscriptionResponse AddSubscriptionResponse
